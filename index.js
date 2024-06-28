c4c4c5b9e69ab1e7ed521b4eb00b8de7d6d04370
Odeh <israelodeh2002@gmail.com>
2024-06-28 11:40:38 +0100

    task 4 completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello,Node.js!")
+const http = require('http');
+
+const fs = require("fs")
+const server = http.createServer((req, res) =>{
+    res.writeHead(200,{'Content-Type' : 'text/plain'});
+    res.end('Hello,World!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`);
+});
